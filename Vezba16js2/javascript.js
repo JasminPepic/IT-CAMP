584c31d9d584a5f1093cb46d824ca75e49538a3d
unknown <pepicjasmin@gmail.com>
2022-06-29 19:11:44 +0200

    Vezba16JS-Vrednosti-String...

---
+++ Vezba16js2/javascript.js
@@ -0,0 +1,11 @@
+let ime = "Jasmin";
+let prezime = "Pepic";
+let imeIprezime = ime +' ' prezime;
+console.log(imeIprezime);
+
+// ovo je isto
+
+let ime = "Jasmin";
+let prezime = "Pepic";
+let imeIprezime = `$(ime) $(prezime)`;
+console.log(imeIprezime);
