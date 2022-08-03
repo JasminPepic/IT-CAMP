f27f243d204f1a21b766bf5519a3dd24cfc2ec2c
unknown <pepicjasmin@gmail.com>
2022-08-03 22:09:04 +0200

    Vezba19

---
+++ Vezba19_js/app.js
@@ -0,0 +1,10 @@
+console.log("Hello world");
+
+//Data type - Primitivni i referentni
+
+//Primitivni:
+//Brojevi - 12, 59, -65, 52.5
+
+//String - pod znacima navoda. Mogu i "Jasmin" i 'Jasmin'
+
+//Boolean - tacno i netacno - true and false
