d1cc169d5c335f2cc98dd4d9fdcc9ab2c7758b16
unknown <pepicjasmin@gmail.com>
2022-08-08 18:45:52 +0200

    Vezba 19 - domaci 1 i 2 v2

--- Vezba19Home2/app.js
+++ Vezba19Home2/app.js
@@ -1,17 +1,17 @@
-number = Number(prompt("Unesite broj"));
-if (number === 1) {
+broj = Number(prompt("Unesite broj"));
+if (broj === 1) {
   console.log("Danas je ponedeljak");
-} else if (number === 2) {
+} else if (broj === 2) {
   console.log("Danas je utorak");
-} else if (number === 3) {
+} else if (broj === 3) {
   console.log("Danas je sreda");
-} else if (number === 4) {
+} else if (broj === 4) {
   console.log("Danas je cetvrtak");
-} else if (number === 5) {
+} else if (broj === 5) {
   console.log("Danas je petak");
-} else if (number === 6) {
+} else if (broj === 6) {
   console.log("Danas je subota");
-} else if (number === 7) {
+} else if (broj === 7) {
   console.log("Danas je nedelja");
 } else {
   console.log("Uneli ste pogresan broj, molim vas da unesete broj od 1 do 7");
