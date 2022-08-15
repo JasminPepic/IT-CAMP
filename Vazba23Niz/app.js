d465651f4aaa5d037232e65afe75dbb83b25c0af
unknown <pepicjasmin@gmail.com>
2022-08-15 21:11:16 +0200

    Vezba 23 Niz

---
+++ Vazba23Niz/app.js
@@ -0,0 +1,58 @@
+// let imena = ["jasmin", "adnan", "emina", "5", "qqwrf"];
+
+// console.log(imena);
+
+// console.log(imena[0]);
+// console.log(imena[1]);
+// console.log(imena[2]);
+// console.log(imena[3]);
+// console.log(imena[4]);
+
+// for (i = 0; i < imena.length; i++) {
+//   console.log(imena[i]);
+// }
+
+// for (i = imena.length - 1; i >= 0; i--) {
+//   console.log(imena[i]);
+// }
+// imena.length;
+
+// const niz = ["ime", "prezime"];
+// let imeprezime = "";
+
+// for (i = 0; i < 1; i++) {
+//   console.log(niz[i] + " " + niz[i + 1]);            prvi nacin
+// }
+
+// for (const el of niz) {
+//   imeprezime += el + " ";    drugi nacin
+// }
+// console.log(imeprezime);
+
+// broj = 5;
+// unos = Number(prompt("Unesite broj"));
+
+// while (unos !== 0) {
+//   if (broj === unos) {
+//     console.log("Pogodili ste broj");
+//     break;
+//   }
+//   if (unos > broj) {
+//     console.log("Broj je veci");
+//   } else {
+//     console.log("Broj je manji");
+//   }
+//   unos = Number(prompt("Unesite novi broj"));
+// }
+
+// let array = [];
+
+// while (true) {
+//   let unos = Number(prompt("unesi broj"));
+//   if (unos % 3 === 0 && unos % 5 === 0) {
+//     break;
+//   } else {
+//     array.push(unos);
+//   }
+// }
+// console.log(array);
